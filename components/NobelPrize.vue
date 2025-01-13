9efa14f6e686e70435c0c77929d7ce8bcb6451cc
AndrewCrescencio <andrew_workstation@hotmail.com>
2025-01-12 22:57:22 -0300

    feat: NobelPrize card component

--- components/NobelPrize.vue
+++ components/NobelPrize.vue
@@ -1,12 +1,57 @@
 <template>
-	<div>
-		<div>{{prize.awardYear}}</div>
-		<div>{{prize.category.en}}</div>
+	<div class="nobel-prize-card text-sm">
+		<p class="uppercase"><strong class="font-bold">{{prize.awardYear}}</strong> Nobel Prize for</p>
+		<p class="font-medium category-name">{{prize.category.en}}</p>
+		<template v-if="formattedLaureateNames">
+			<p class="uppercase">Awarded to</p>
+			<p class="font-medium laureate-names">{{ formattedLaureateNames }}</p>
+		</template>
 	</div>
 </template>
 
 <script setup lang="ts">
-defineProps(['prize'])
+type NobelPrizeCategoryFullName = {
+	en: string;
+	no: string;
+	se: string;
+}
+
+interface Laureate {
+	fullName: {
+		en: string;
+	};
+	knownName: {
+		en: string;
+	};
+	orgName: {
+		en: string;
+	};
+}
+
+interface NobelPrize {
+	awardYear: string;
+	dateAwarded: string;
+	category: {
+		en: string;
+	};
+	categoryFullName: NobelPrizeCategoryFullName;
+	laureates?: Laureate[];
+}
+
+interface Props {
+	prize: NobelPrize;
+}
+
+const props = defineProps<Props>();
+
+const formattedLaureateNames = computed(() => {
+	const validLaureates = props.prize.laureates?.filter(l => {
+		return l?.knownName?.en || l?.fullName?.en || l?.orgName?.en;
+	}) ?? [];
+	return validLaureates.map(l => {
+		return l.knownName?.en || l.fullName?.en || l.orgName?.en;
+	}).join(', ');
+});
 
 function removePrize() {
 
@@ -26,5 +71,33 @@ function removePrize() {
 /* You must fully implement the styles for both versions of the card */
 .nobel-prize-card-tailwind{
 
+}
+
+.nobel-prize-card {
+	display: flex;
+	flex-direction: column;
+	gap: 0.5rem;
+	border: 2px solid #e5e7eb;
+	border-radius: 0.375rem;
+	padding: 1rem;
+	width: 100%;
+	height: 100%;
+	min-height: 150px;
+}
+
+.category-name {
+	overflow: hidden;
+	text-overflow: ellipsis;
+	display: -webkit-box;
+	-webkit-line-clamp: 2;
+	-webkit-box-orient: vertical;
+}
+
+.laureate-names {
+	overflow: hidden;
+	text-overflow: ellipsis;
+	display: -webkit-box;
+	-webkit-line-clamp: 3;
+	-webkit-box-orient: vertical;
 }
 </style>
